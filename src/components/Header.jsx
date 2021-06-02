9e87f1973676260a0df3b0b3efba894443105e78
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-06-02 17:25:27 +0200

    adds styles to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,19 @@ import menuTop from '../images/menu-top.svg'
 export default class Header extends Component {
     render() {
         return (
-            <header>
-                <nav className="navBar">
+            <header className="App__header">
+                <nav className="App__header__navBar container">
                     <img src={logo} alt="Ironhack logo" />
                     <img src={menuTop} alt="mobile menu icon"/>
                 </nav>
-                <div className="header__content">
-                    <h1>Say hello to ReactJS</h1>
-                    <p>You will learn how to use the most popular frontend library,
-                        and become a super Ninja developer.
-                    </p>
-                    <button>Awesome!</button>
+                 <div className="container">
+                    <div className='App__header__content'>
+                        <h1>Say hello to ReactJS</h1>
+                        <p>You will learn how to use the most popular frontend library,
+                                and become a super Ninja developer.
+                        </p>
+                        <button className="button">Awesome!</button>
+                    </div>
                 </div>
             </header>
         )
